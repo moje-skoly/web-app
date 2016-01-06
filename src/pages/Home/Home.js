d122b1ee9cb72fe871f252d933b766a27578b6c0
Marek Lisy <marek.lisy@centrum.cz>
2016-01-07 00:40:10 +0100

    Home subtitle correction

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => (
   <div>
     <div className={styles.homepage}>
       <h1><strong>Najděte školu</strong> pro své děti.</h1>
-      <p>Naše škola je otevřená databáze mateřských, základních a středních škol na území České republiky, kterou spravují školy a rodiče.</p>
+      <p><strong>Moje školy</strong> je databáze mateřských, základních a středních škol na území České republiky sestavená z otevřených dat veřejných institucí.</p>
     </div>
     <HomepageSearch />
     <HowItWorks />
