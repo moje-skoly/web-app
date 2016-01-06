19c0bef748343ae00d879bdba807273a79df2a93
Marek Lisy <marek.lisy@centrum.cz>
2016-01-07 00:02:26 +0100

    Unit type

--- src/components/UnitDetail/UnitDetail.js
+++ src/components/UnitDetail/UnitDetail.js
@@ -9,34 +9,49 @@ const UnitDetail = ({
     metadata,
     sections
   }
-}) => (
-  <div className={styles.unit}>
-    <h3 className={styles.unitTitle}>{unitType}</h3>
-    <div className={styles.body}>
-      {metadata
-        && <MetaData comparison={schoolMetadata} data={metadata} />}
+}) => {
+  const getUnitType = (type) => {
+    switch (type) {
+    case 'materska_skola':
+      return 'Mateřská škola';
+    case 'zakladni_skola':
+      return 'Základní škola';
+    case 'stredni_skola':
+      return 'Střední škola';
+    default:
+      return type + 'original';
+    }
+  };
 
-      {sections
-        && sections.map((section, sectionId) => (
-        <div key={sectionId}>
-          <h4 className={styles.sectionTitle}>{section.title}</h4>
-          <table>
-            <tbody>
-              {section.information.map((info, infoId) => {
-                const { key, value } = info;
-                return (
-                  <tr key={infoId}>
-                    <th className={styles.question}>{key}{':'}</th>
-                    <td className={styles.answer}>{value}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      ))}
+  return (
+    <div className={styles.unit}>
+      <h3 className={styles.unitTitle}>Hello {getUnitType(unitType)}</h3>
+      <div className={styles.body}>
+        {metadata
+          && <MetaData comparison={schoolMetadata} data={metadata} />}
+
+        {sections
+          && sections.map((section, sectionId) => (
+          <div key={sectionId}>
+            <h4 className={styles.sectionTitle}>{section.title}</h4>
+            <table>
+              <tbody>
+                {section.information.map((info, infoId) => {
+                  const { key, value } = info;
+                  return (
+                    <tr key={infoId}>
+                      <th className={styles.question}>{key}{':'}</th>
+                      <td className={styles.answer}>{value}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default UnitDetail;
